b012f802b79a6becec6871f34d1e6ea4e48e028a
junngo <myeongjun.ko@gmail.com>
2020-05-29 14:29:10 +0900

    created creating chat room api

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chat Chat Django Server
+Chat Chat Server Using Django-channels for React Native
+
+## Api Stories
+- [x] Create Account (Sign Up)
+- [x] Log In
+- [x] Create Chat Room
+- [] Get Chat Room List
+- [] Get Chat Contents (Pick a chat room)
--- chat_chat/chat/views.py
+++ chat_chat/chat/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
 from . import models, serializers
+from chat_chat.users import models as user_models
 
 def index(request):
     return render(request, 'chat/index.html')
@@ -12,9 +14,30 @@ def room(request, room_name):
         'room_name': room_name
     })
 
+
 class Room(APIView):
     def get(self, request, format=None):
         rooms = models.Room.objects.all()
         serializer = serializers.RoomSerializer(rooms, many=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        user = request.user
+
+        try:
+            found_admin_user = user_models.User.objects.get(username='admin')
+
+        except user_models.User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        new_contact = models.Contact.objects.create(user=user)
+        new_contact.friends.add(found_admin_user)
+
+        new_room = models.Room.objects.create()
+        new_room.participants.add(new_contact)
+
+        new_contact.save()
+        new_room.save()
+
+        return Response(status=status.HTTP_201_CREATED)
